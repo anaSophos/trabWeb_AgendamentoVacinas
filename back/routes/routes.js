41696c3e4a701cc95457405f52abb8569d9032e3
Gleiciane Alves <112650788+GleicianeFernandes@users.noreply.github.com>
2023-11-03 21:00:15 -0300

    Update routes.js
    atalização da rota da vacina

--- back/routes/routes.js
+++ back/routes/routes.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import userRoutes from './routePaths/userRoutes.js'
 import hospitalRoutes from './routePaths/hospitalRoutes.js'
+import vaccineRoutes from './routePaths/vaccineRoutes.js'
 
 const router = express.Router()
 
 router.use('/user', userRoutes)
 router.use('/hosp', hospitalRoutes)
+router.use('/user', vaccineRoutes)
 
 export default router
